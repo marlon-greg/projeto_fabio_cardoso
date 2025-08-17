e00acb38de7826249c9a5c49f3e3320e23507b89
marlongreg <marlonfanger@gmail.com>
2025-08-17 14:07:14 -0300

    correções github

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+  // Adicione esta linha:
+  base: '/projeto_fabio_cardoso/', 
+})
